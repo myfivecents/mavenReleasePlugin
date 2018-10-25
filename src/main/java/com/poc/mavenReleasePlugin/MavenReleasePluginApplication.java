976911b9c49c5baa6075d856e4d6fc2ee536d219
Nickell, Jacob <jacob.nickell@accenture.com>
2018-10-25 15:39:06 -0400

    initial commit

---
+++ src/main/java/com/poc/mavenReleasePlugin/MavenReleasePluginApplication.java
@@ -0,0 +1,12 @@
+package com.poc.mavenReleasePlugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenReleasePluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenReleasePluginApplication.class, args);
+	}
+}
